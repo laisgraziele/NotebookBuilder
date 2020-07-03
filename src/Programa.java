06953f0c2acd923af56cb6fcc66232944a74e89a
Lais Graziele <58828160+laisgraziele@users.noreply.github.com>
2020-07-02 21:31:19 -0300

    Update Programa.java

--- src/Programa.java
+++ src/Programa.java
@@ -4,7 +4,7 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		Director diretor = new Director();
-		
+		notebookSonic.criaNotebook();
 		NotebookBuilder notebookSonic = new SonicMaster();
 		
 		diretor.setNotebookBuilder(notebookSonic);
